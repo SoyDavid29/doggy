49af9433163fc26bbc174c4bbd58a3491ff7ef95
DavidRodriguez <studiandoprogramacion@gmail.com>
2025-05-23 19:44:12 -0600

    first changes in sidebar

---
+++ src/Components/Sidebar.jsx
@@ -0,0 +1,19 @@
+export default function Sidebar(){
+    return(
+        <section className="sidebar">
+            <div className="bar">
+
+                <div className="btn-menu">
+                    <div className="stick"></div>
+                    <div className="stick"></div>
+                    <div className="stick"></div>
+                </div>
+
+                <h1>LOGO</h1>
+
+                <img src="" alt="logo.whatsapp" />
+
+            </div>
+        </section>
+    )
+}
